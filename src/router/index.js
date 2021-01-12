0a9818f6fd82b169c30e7218c1b5eb3d86b2bcb7
suntaojojo <suntaojojo@jojo.com>
2021-01-12 10:20:01 +0800

    'jojotao3'

--- src/router/index.js
+++ src/router/index.js
@@ -4,12 +4,9 @@ import First from '@/router/modules/first/first'
 import Cart from '@/router/modules/cart/cart'
 import Mystudy from '@/router/modules/mystudy/mystudy'
 import Personal from '@/router/modules/personal/personal'
-<<<<<<< HEAD
 import modeList from '@/router/modules/all/modeList'
-=======
 import Login from '@/router/modules/login/login'
 import Detail from '@/router/modules/detail/detail'
->>>>>>> 0a24eb7036cd6f55077b48f2c1a2a2f083a93d3d
 Vue.use(VueRouter)
 
 const routes = [
@@ -17,12 +14,9 @@ const routes = [
   Cart,
   Mystudy,
   Personal,
-<<<<<<< HEAD
-  modeList
-=======
+  modeList,
   Login,
   Detail,
->>>>>>> 0a24eb7036cd6f55077b48f2c1a2a2f083a93d3d
 ]
 
 const router = new VueRouter({
